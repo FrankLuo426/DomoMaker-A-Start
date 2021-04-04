36f5203f86a36dc7a53b6614b1fd9a2e61c82c92
FrankLuo426 <33915033+FrankLuo426@users.noreply.github.com>
2021-04-04 23:45:42 +0800

    A

---
+++ server/controllers/Domo.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+const makerPage = (req, res) => {
+    res.render('app');
+};
+
+module.exports.makerPage = makerPage;
---
+++ server/router.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+const controllers = require('./controllers');
+
+const router = (app) => {
+    app.get('/login', controllers.Account.loginPage);
+    app.post('/login', controllers.Account.login);
+    app.get('/signup', controllers.Account.signupPage);
+    app.post('/signup', controllers.Account.signup);
+    app.get('/logout', controllers.Account.logout);
+    app.get('/maker', controllers.Domo.makerPage);
+    app.get('/', controllers.Account.loginPage);
+};
+
+module.exports = router;
