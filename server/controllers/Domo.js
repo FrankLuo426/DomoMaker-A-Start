4c25722938d0c6ad15e984c77ea9bc169761c204
FrankLuo426 <33915033+FrankLuo426@users.noreply.github.com>
2021-04-08 02:17:55 +0800

    b

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,66 +1,49 @@
-/* eslint-disable no-console */
-/* eslint-disable indent */
 /* eslint-disable linebreak-style */
-/* eslint-disable eol-last */
+/* eslint-disable indent */
+const models = require('../models');
 
+const Domo = models.Domo;
 
-// const { makerPage } = require('./Domo');
-//
-
-// const makerPage = (req, res) => {
-//     res.render('app');
-// };
-
-const mongoose = require('mongoose');
-
-//mongoose.Promise = global.Promise;
-const _ = require('underscore');
-
-let DomoModel = {};
-//const { Domo } = models;
-
-// mongoose.Types.ObjectID is a function that
-// converts string ID to real mongo ID
-const convertld = mongoose.Types.Objectld;
-const setName = (name) => _.escape(name).trim();
-
-const DomoSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        set: setName,
-    },
-    age: {
-        type: Number,
-        min: 0,
-        required: true,
-    },
-    owner: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: 'Account',
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now,
-    },
-});
-
-
-
-DomoSchema.statics.toAPI = (doc) => ({
-    name: doc.name,
-    age: doc.age,
-});
-DomoSchema.statics.findByOwnor = (ownerld, callback) => {
-    const search = {
-        owner: convertld(ownerld),
+const makeDomo = (req, res) => {
+    if (!req.body.name || !req.body.age) {
+        return res.status(400).json({
+            error: 'RAWR! Both name and age are required',
+        });
+    }
+    const domoData = {
+        name: req.body.name,
+        age: req.body.age,
+        owner: req.session.account._id,
     };
-    return DomoModel.find(search).select('name age').lean().exec(callback);
+    const newDomo = new Domo.DomoModel(domoData);
+    const domoPromise = newDomo.save();
+    domoPromise.then(() => res.json({
+        redirect: '/maker',
+    }));
+    domoPromise.catch((err) => {
+        console.log(err);
+        if (err.code === 11000) {
+            return res.status(400).json({
+                error: 'Domo already exists.',
+            });
+        }
+        return res.status(400).json({
+            error: 'An error occurred ',
+        });
+    });
+    return domoPromise;
 };
-DomoModel = mongoose.model('Domo', DomoSchema);
-module.exports.DomoModel = DomoModel;
-module.exports.DomoSchema = DomoSchema;
 
+const makerPage = (req, res) => {
+    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ error: 'Am error occurred' });
+        }
 
+        return res.render('app', { domos: docs });
+    });
+};
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
--- server/models/Domo.js
+++ server/models/Domo.js
@@ -1,48 +1,56 @@
-/* eslint-disable linebreak-style */
+
+/* eslint-disable no-console */
 /* eslint-disable indent */
-const models = require('./models');
-const Domo = models.Domo;
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
 
-const makeDomo = (req, res) => {
-    if (!req.body.name || !req.body.age) {
-        return res.status(400).json({
-            error: 'RAWR! Both name and age are required',
-        });
-    }
-    const domoData = {
-        name: req.body.name,
-        age: req.body.age,
-        owner: req.session.account._id,
+const mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+const _ = require('underscore');
+
+let DomoModel = {};
+// const { Domo } = models;
+
+// mongoose.Types.ObjectID is a function that
+// converts string ID to real mongo ID
+const convertId = mongoose.Types.ObjectId;
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        set: setName,
+    },
+    age: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'Account',
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+
+DomoSchema.statics.toAPI = (doc) => ({
+    name: doc.name,
+    age: doc.age,
+});
+DomoSchema.statics.findByOwnor = (ownerld, callback) => {
+    const search = {
+        owner: convertId(ownerld),
     };
-    const newDomo = new Domo.DomoModel(domoData);
-    const domoPromise = newDomo.save();
-    domoPromise.then(() => res.json({
-        redirect: '/maker',
-    }));
-    domoPromise.catch((err) => {
-        console.log(err);
-        if (err.code === 11000) {
-            return res.status(400).json({
-                error: 'Domo already exists.',
-            });
-        }
-        return res.status(400).json({
-            error: 'An error occurred ',
-        });
-    });
-    return domoPromise;
+    return DomoModel.find(search).select('name age').lean().exec(callback);
 };
-
-const makerPage = (req, res) => {
-    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-        if (err) {
-            console.log(err);
-            return res.status(400).json({ error: 'Am error occurred' });
-        }
-
-        return res.render('app', { domos: docs });
-    });
-};
-
-module.exports.makerPage = makerPage;
-module.exports.make = makeDomo;
+DomoModel = mongoose.model('Domo', DomoSchema);
+module.exports.DomoModel = DomoModel;
+module.exports.DomoSchema = DomoSchema;
