92bafc493a5047186184c5a01a6905a289e9a9e7
FrankLuo426 <33915033+FrankLuo426@users.noreply.github.com>
2021-04-08 02:00:21 +0800

    all

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -15,8 +15,7 @@ const mongoose = require('mongoose');
 
 //mongoose.Promise = global.Promise;
 const _ = require('underscore');
-const models = require('../models');
-const Domo = models.Domo;
+
 let DomoModel = {};
 //const { Domo } = models;
 
@@ -48,46 +47,7 @@ const DomoSchema = new mongoose.Schema({
     },
 });
 
-const makeDomo = (req, res) => {
-    if (!req.body.name || !req.body.age) {
-        return res.status(400).json({
-            error: 'RAWR! Both name and age are required',
-        });
-    }
-    const domoData = {
-        name: req.body.name,
-        age: req.body.age,
-        owner: req.session.account._id,
-    };
-    const newDomo = new Domo.DomoModel(domoData);
-    const domoPromise = newDomo.save();
-    domoPromise.then(() => res.json({
-        redirect: '/maker',
-    }));
-    domoPromise.catch((err) => {
-        console.log(err);
-        if (err.code === 11000) {
-            return res.status(400).json({
-                error: 'Domo already exists.',
-            });
-        }
-        return res.status(400).json({
-            error: 'An error occurred ',
-        });
-    });
-    return domoPromise;
-};
 
-const makerPage = (req, res) => {
-    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-        if (err) {
-            console.log(err);
-            return res.status(400).json({ error: 'Am error occurred' });
-        }
-
-        return res.render('app', { domos: docs });
-    });
-};
 
 DomoSchema.statics.toAPI = (doc) => ({
     name: doc.name,
@@ -104,5 +64,3 @@ module.exports.DomoModel = DomoModel;
 module.exports.DomoSchema = DomoSchema;
 
 
-module.exports.makerPage = makerPage;
-module.exports.make = makeDomo;
